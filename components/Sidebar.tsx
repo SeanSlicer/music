05acbce6e25ba4abd55d1a6c16bc963e86c36c5a
Sean Slicer <722sas17@gmail.com>
2023-08-17 03:16:19 -0500

    Sidear now rendering children

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ interface SidebarProps {
   children: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }): any => {
+const Sidebar = ({ children }: SidebarProps) => {
   const pathname = usePathname();
 
   const routes = useMemo(
@@ -32,19 +32,21 @@ const Sidebar: React.FC<SidebarProps> = ({ children }): any => {
     [pathname]
   );
 
-  return;
-  <div className="flex h-full">
-    <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-      <Box>
-        <div className="flex flex-col gap-y-4 px-5 py-4">
-          {routes.map((item) => (
-            <SidebarItem key={item.label} {...item} />
-          ))}
-        </div>
-      </Box>
-      <Box className="overflow-y-auto h-full">Song Library</Box>
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
+      </div>
+      <main>{children}</main>
     </div>
-  </div>;
+  );
 };
 
 export default Sidebar;
