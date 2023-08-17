55024f661ec2fbe03f7c0cc7d086bd599b8aa4d7
Sean Slicer <722sas17@gmail.com>
2023-08-17 02:16:06 -0500

    icons added and Box implemented

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,22 +2,28 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC<SidebarProps> = ({ children }): any => {
   const pathname = usePathname();
 
   const routes = useMemo(
     () => [
       {
+        icon: HiHome,
         label: "Home",
         active: pathname !== "/search",
         href: "/",
       },
       {
+        icon: BiSearch,
         label: "Search",
         active: pathname === "/search",
         href: "/search",
@@ -26,7 +32,19 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     [pathname]
   );
 
-  return <div>{children}</div>;
+  return;
+  <div className="flex h-full">
+    <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+      <Box>
+        <div className="flex flex-col gap-y-4 px-5 py-4">
+          {routes.map((item) => (
+            <SidebarItem key={item.label} {...item} />
+          ))}
+        </div>
+      </Box>
+      <Box className="overflow-y-auto h-full">Song Library</Box>
+    </div>
+  </div>;
 };
 
 export default Sidebar;
