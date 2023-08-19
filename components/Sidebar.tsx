d140d2e9b9e2ebcc1eaed5cb01d3cb3ffb0b545a
Sean Slicer <722sas17@gmail.com>
2023-08-18 20:45:00 -0500

    Sidebar updated wip

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,12 +6,13 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
-const Sidebar = ({ children }: SidebarProps) => {
+const Sidebar: React.FC<SidebarProps> = ({ children }: SidebarProps) => {
   const pathname = usePathname();
 
   const routes = useMemo(
@@ -42,9 +43,11 @@ const Sidebar = ({ children }: SidebarProps) => {
             ))}
           </div>
         </Box>
-        <Box className="overflow-y-auto h-full">Song Library</Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
-      <main>{children}</main>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
